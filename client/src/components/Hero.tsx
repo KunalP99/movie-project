eb5f7e7537cc7a9a742d8d1072accbd7ce813789
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-03-11 23:32:51 +0000

    feat(api): store data in array and display on screen

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react';
 import { getMoviesInTheatre } from '../api/api';
+import IMoviesInTheatre from '../models/IMoviesInTheatre';
 
 const Hero = () => {
-  const [moviesInTheatre, setMoviesInTheatre] = useState();
+  const [moviesInTheatre, setMoviesInTheatre] = useState<IMoviesInTheatre[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
+  // Fetch data from TMDB api
   useEffect(() => {
+    setLoading(true);
     getMoviesInTheatre()
-      .then(data => console.log(data.results))
+      .then(data => {
+        console.log(data.results);
+        setMoviesInTheatre(data.results.slice(0, 6));
+      })
       .catch(err => {
         console.log(err.message);
       })
@@ -18,7 +24,11 @@ const Hero = () => {
 
   return (
     <section className="hero-container">
-      
+      {!loading && moviesInTheatre.map(movie => (
+        <div key={movie.id}>
+          <p>{movie.title}</p>
+        </div>
+      ))}
     </section>
   );
 };
