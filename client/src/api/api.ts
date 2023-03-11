6930e401fcda56798439086535f0fc7681d67d5b
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-03-11 23:19:55 +0000

    refactor(api): add error check for response from api call

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,4 +1,11 @@
 export const getMoviesInTheatre = () => {
-  return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
-    .then(res => res.json());
+  return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=enUS&page=1`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(
+          `ERROR: Status code: ${res.status}`
+        );
+      }
+      return res.json();
+    });
 };
