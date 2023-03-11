1ef1654c8067b497adc0a1f2fc5edf2704f8112c
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-03-11 23:42:59 +0000

    feat(swiper): implement swiper to component

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import { getMoviesInTheatre } from '../api/api';
 import IMoviesInTheatre from '../models/IMoviesInTheatre';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination, Autoplay } from 'swiper';
 
 const Hero = () => {
   const [moviesInTheatre, setMoviesInTheatre] = useState<IMoviesInTheatre[]>([]);
@@ -24,11 +28,19 @@ const Hero = () => {
 
   return (
     <section className="hero-container">
-      {!loading && moviesInTheatre.map(movie => (
-        <div key={movie.id}>
-          <p>{movie.title}</p>
-        </div>
-      ))}
+      <Swiper 
+        pagination={true}
+        modules={[Pagination, Autoplay]}
+        autoplay={{
+          delay: 9000
+        }}
+      >
+        {!loading && moviesInTheatre.map(movie => (
+          <SwiperSlide key={movie.id}>
+            <img src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
