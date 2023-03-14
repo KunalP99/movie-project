82f9b0e537ace036232375a2363bfe047114d592
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-03-14 17:33:29 +0000

    feat(swiper): setup swiper for trending component

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import { getTrendingMovies } from '../api/api';
 import ITrendingMovies from '../models/ITrendingMovies';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation, Autoplay } from 'swiper';
 
 const Trending = () => {
   const [trendingMovies, setTrendingMovies] = useState<ITrendingMovies[]>();
@@ -11,7 +15,7 @@ const Trending = () => {
     getTrendingMovies()
       .then(data => {
         console.log(data.results);
-        setTrendingMovies(data.results.slice(0, 10));
+        setTrendingMovies(data.results.slice(0, 15));
       })
       .catch(err => {
         console.log(err.message);
@@ -24,6 +28,17 @@ const Trending = () => {
   return (
     <section className="trending-container">
       <h2>Trending</h2>
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        slidesPerView={4.5}
+      >
+        {!loading && trendingMovies?.map(movie => (
+          <SwiperSlide key={movie.id}>
+            <img src={`http://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="" loading='lazy'/>
+          </SwiperSlide>   
+        ))}
+      </Swiper>
     </section>
   );
 };
