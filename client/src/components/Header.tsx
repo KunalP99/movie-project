2fab4b852c314dc51bde22d5d7baba488b148d73
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-03-14 14:48:54 +0000

    feat(header): style header for mobile view

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,19 @@
 import HambugerIcon from '../images/hamburger-menu.svg';
+import SearchIcon from '../images/sidebar/search-icon.svg';
+import WatchlistIcon from '../images/sidebar/watchlist-icon.svg';
+import HistoryIcon from '../images/sidebar/history-icon.svg';
+import ProfileIcon from '../images/sidebar/profile-icon.svg';
+import CloseIcon from '../images/sidebar/x.svg';
 
 const Header = () => {
+  const toggleMenu = () => {
+    const navbarLinks = document.querySelector('.navbar-links');
+    const xBtn = document.querySelector('.close-sidebar-btn');
+
+    // Show X icon and Hamburger icon
+    navbarLinks?.classList.toggle('active');
+    console.log('click');
+  };
   return (
     <header>
       <nav>
@@ -10,14 +23,27 @@ const Header = () => {
           </a>
         </div>
         <div className='navbar-links'>
+          <button className='close-sidebar-btn'><img src={CloseIcon} alt="Close sidebar" /></button>
           <ul>
-            <li><a href="#">Search</a></li>
-            <li><a href="#">Watchlist</a></li>
-            <li><a href="#">History</a></li>
-            <li><a href="#">Name</a></li>
+            <li>
+              <img src={SearchIcon} alt="Search for a movie" />
+              <a href="#">Search</a>
+            </li>
+            <li>
+              <img src={WatchlistIcon} alt="View your watchlist" />
+              <a href="#">Watchlist</a>
+            </li>
+            <li>
+              <img src={HistoryIcon} alt="View your watch history" />
+              <a href="#">History</a>
+            </li>
+            <li>
+              <img src={ProfileIcon} alt="View your profile" />
+              <a href="#">Name</a>
+            </li>
           </ul>
         </div>
-        <button className='hamburger-btn' type='button'><img src={HambugerIcon} alt="Open sidebar" /></button>
+        <button className='hamburger-btn' type='button' onClick={toggleMenu}><img src={HambugerIcon} alt="Open sidebar" /></button>
       </nav>
     </header>
   );
