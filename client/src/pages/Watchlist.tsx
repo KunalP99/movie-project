d7b1d778398b2ec80dc718d03cf95fb139fa59ee
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-04-06 13:52:45 +0100

    feat(watchlist): click on movie to redirect user to movie details page

--- client/src/pages/Watchlist.tsx
+++ client/src/pages/Watchlist.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useContext } from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 // Models
 import { IHandleGetWatchlistMovies } from '../models/IWatchlist';
@@ -14,18 +15,14 @@ interface Props {
 const Watchlist = ({ watchlist, loading} : Props) => {
   const { user } = useContext(UserContext);
 
-  useEffect(() => {
-    console.log(watchlist);
-  }, [watchlist]);
-
   return (
     <section className="watchlist-container">
       <h2>Your Watchlist</h2>
       <div className='watchlist-movies-container'>
         {!loading && watchlist.filter(person => person.user_id === user.sub).map(movie => (
-          <div className='watchlist-movie-container' key={movie.movieId} title={movie.title}>
+          <Link to={`/movie/${movie.movieId}`} className='watchlist-movie-container' key={movie.movieId} title={movie.title}>
             <img src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`} alt={movie.title} />
-          </div>
+          </Link>
         ))}
       </div>
 
