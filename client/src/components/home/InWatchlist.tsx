455e3cb168c58760a4fffd73778e791d5a65ec08
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-04-28 17:33:02 +0100

    refactor(in-watchlist): remove unecessary imports

--- client/src/components/home/InWatchlist.tsx
+++ client/src/components/home/InWatchlist.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef} from 'react';
+import { useRef} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper';
 import 'swiper/css';
